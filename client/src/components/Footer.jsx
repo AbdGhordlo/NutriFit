33e0a413d5856e8eb1e520415d80c59a8b9b3979
AbdGhordlo <90023620+AbdGhordlo@users.noreply.github.com>
2025-01-30 15:23:06 +0300

    Meal Planner Front End

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,29 +1,39 @@
-import React, { useState } from 'react';
-import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube } from 'react-icons/fa'; // Importing from react-icons
+import React, { useState } from "react";
+import { FaFacebookF, FaInstagram, FaYoutube } from "react-icons/fa";
 import { AiOutlineX } from "react-icons/ai";
-import Logo from '../assets/imgs/logo-no-padding.png';
-import { styles } from './styles/FooterStyles';
+import Logo from "../assets/imgs/logo-no-padding.png";
+import { styles } from "./styles/FooterStyles";
 
 export default function Footer() {
   const [hoveredIcon, setHoveredIcon] = useState(null);
+  const [hoveredLink, setHoveredLink] = useState(null);
 
   const getHoverStyle = (icon) => {
     if (hoveredIcon === icon) {
       switch (icon) {
-        case 'facebook':
-          return { color: '#1877F2' };
-        case 'x':
-          return { color: 'black' };
-        case 'instagram':
-          return { color: '#E4405F' };
-        case 'youtube':
-          return { color: '#FF0000' };
+        case "facebook":
+          return { color: "#1877F2" };
+        case "x":
+          return { color: "black" };
+        case "instagram":
+          return { color: "#E4405F" };
+        case "youtube":
+          return { color: "#FF0000" };
         default:
           return {};
       }
     }
-    return { color: '#9ca3af' }; // Default color
+    return { color: "#9ca3af" };
   };
+
+  /* Social icons we'll add to the bottom right of the page. We created an array of 
+  objects so that we can map over them (It makes it look cleaner). */
+  const socialIcons = [
+    { id: "facebook", component: <FaFacebookF /> },
+    { id: "x", component: <AiOutlineX /> },
+    { id: "instagram", component: <FaInstagram /> },
+    { id: "youtube", component: <FaYoutube /> },
+  ];
 
   return (
     <footer style={styles.footer}>
@@ -33,55 +43,46 @@ export default function Footer() {
             <img src={Logo} style={styles.logo} alt="Logo" />
             <span style={styles.brandName}>NutriFit</span>
           </div>
-          <span style={styles.copyright}>© 2025 NutriFit. All rights reserved.</span>
+          <span style={styles.copyright}>
+            © 2025 NutriFit. All rights reserved.
+          </span>
         </div>
 
         <div style={styles.rightSection}>
           <div style={styles.links}>
-            <a href="#" style={styles.link}>
-              About
-            </a>
-            <a href="#" style={styles.link}>
-              Contact
-            </a>
-            <a href="#" style={styles.link}>
-              Privacy
-            </a>
+            {/* Notice: when the mouse hovers over any one of the links, its color will change */}
+            {["About", "Contact", "Privacy"].map((item, index) => (
+              <a
+                key={index}
+                href="#"
+                style={{
+                  ...styles.link,
+                  color: hoveredLink === index ? "#4d7051" : "#4b5563",
+                }}
+                onMouseEnter={() => setHoveredLink(index)}
+                onMouseLeave={() => setHoveredLink(null)}
+              >
+                {item}
+              </a>
+            ))}
           </div>
 
           <div style={styles.socialLinks}>
-            <a
-              href="#"
-              style={{ ...styles.socialIcon, ...getHoverStyle('facebook') }}
-              onMouseEnter={() => setHoveredIcon('facebook')}
-              onMouseLeave={() => setHoveredIcon(null)}
-            >
-              <FaFacebookF />
-            </a>
-            <a
-              href="#"
-              style={{ ...styles.socialIcon, ...getHoverStyle('x') }}
-              onMouseEnter={() => setHoveredIcon('x')}
-              onMouseLeave={() => setHoveredIcon(null)}
-            >
-              <AiOutlineX />
-            </a>
-            <a
-              href="#"
-              style={{ ...styles.socialIcon, ...getHoverStyle('instagram') }}
-              onMouseEnter={() => setHoveredIcon('instagram')}
-              onMouseLeave={() => setHoveredIcon(null)}
-            >
-              <FaInstagram />
-            </a>
-            <a
-              href="#"
-              style={{ ...styles.socialIcon, ...getHoverStyle('youtube') }}
-              onMouseEnter={() => setHoveredIcon('youtube')}
-              onMouseLeave={() => setHoveredIcon(null)}
-            >
-              <FaYoutube />
-            </a>
+            {/* Notice: here we're mapping through the social links, and for each one, we check
+            whether we're hovering over it. If we're, we set the hoveredIcon to the id of 
+            element we're hovering over, which triggers a rerender, which changes the color
+            when getHoverStyle is called below */}
+            {socialIcons.map(({ id, component }) => (
+              <a
+                key={id}
+                href="#"
+                style={{ ...styles.socialIcon, ...getHoverStyle(id) }}
+                onMouseEnter={() => setHoveredIcon(id)}
+                onMouseLeave={() => setHoveredIcon(null)}
+              >
+                {component}
+              </a>
+            ))}
           </div>
         </div>
       </div>
--- client/src/components/styles/MealPlannerStyles.ts
+++ client/src/components/styles/MealPlannerStyles.ts
@@ -1,13 +1,18 @@
 export const styles = {
   container: {
-    padding: '24px'
+    padding: '20px',
+    maxWidth: '1000px', // I think this much is enough
+    width: '80%',
+    margin: '0 auto',
   },
   mainContainer: {
     backgroundColor: 'white',
     borderRadius: '8px',
     boxShadow: '0 1px 2px rgba(0, 0, 0, 0.05)',
     padding: '24px',
-    marginBottom: '24px'
+    marginBottom: '24px',
+    // maxWidth: '1000px',
+    // width: '100%'
   },
   title: {
     fontSize: '24px',
@@ -15,18 +20,18 @@ export const styles = {
     color: '#1f2937',
     marginBottom: '24px'
   },
-  dayContainer: (isToday) => ({
-    position: 'relative',
-    borderRadius: '8px',
-    padding: '24px',
-    transition: 'all 0.3s',
-    border: isToday ? '2px solid #7ec987' : '1px solid #f3f4f6',
-    boxShadow: isToday ? '0 0 15px rgba(126, 201, 135, 0.2)' : 'none'
+  dayContainer: (isToday: boolean): React.CSSProperties => ({
+    // position: "relative",
+    borderRadius: "8px",
+    padding: "24px",
+    transition: "all 0.3s",
+    border: isToday ? "2px solid #7ec987" : "2px solid #f3f4f6",
+    boxShadow: isToday ? "0 0 15px rgba(126, 201, 135, 0.2)" : "none"
   }),
   dayHeader: {
-    textAlign: 'center',
-    marginBottom: '32px'
-  },
+    textAlign: "center",
+    marginBottom: "32px",
+  } as React.CSSProperties,
   dayName: {
     fontSize: '20px',
     fontWeight: '600',
@@ -38,22 +43,19 @@ export const styles = {
   mealsList: {
     display: 'flex',
     flexDirection: 'column',
-    gap: '16px'
-  },
+    gap: '14px' // Reduced a bit
+  } as React.CSSProperties,
   mealItem: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'space-between',
     padding: '16px',
     borderRadius: '8px',
-    border: '1px solid #f3f4f6',
+    // border: '1px solid #f3f4f6',
     transition: 'border-color 0.2s',
-    ':hover': {
-      borderColor: '#e5e7eb'
-    }
   },
   mealInfo: {
-    flex: 1
+    flex: 1 //this doesn't seem to be doing anything, but I'm not 100% sure
   },
   mealName: {
     fontWeight: '500',
@@ -80,8 +82,11 @@ export const styles = {
     fontSize: '14px'
   },
   macroItem: {
-    textAlign: 'center'
-  },
+    // textAlign: 'center'
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  } as React.CSSProperties,
   macroValue: {
     display: 'block',
     fontWeight: '500',
@@ -101,45 +106,39 @@ export const styles = {
     padding: '8px',
     borderRadius: '50%',
     transition: 'all 0.2s',
-    color: disabled ? '#d1d5db' : '#7ec987',
-    cursor: disabled ? 'not-allowed' : 'pointer',
-    ':hover': !disabled ? {
-      backgroundColor: 'rgba(126, 201, 135, 0.1)'
-    } : {}
+    border: 'none',
+    cursor: disabled ? 'default' : 'pointer',
+    height: '40px',
+    width: '40px',
   }),
+
   buttonsContainer: {
     display: 'flex',
     justifyContent: 'center',
-    gap: '16px'
+    gap: '16px',
   },
+
   generateButton: {
     display: 'flex',
     alignItems: 'center',
     gap: '8px',
     padding: '12px 24px',
-    backgroundColor: '#7ec987',
     color: 'white',
     borderRadius: '8px',
     border: 'none',
     cursor: 'pointer',
     transition: 'background-color 0.2s',
-    ':hover': {
-      backgroundColor: '#6db776'
-    }
   },
+
   editButton: {
     display: 'flex',
     alignItems: 'center',
     gap: '8px',
     padding: '12px 24px',
-    backgroundColor: 'transparent',
     color: '#4d7051',
     borderRadius: '8px',
     border: '2px solid #7ec987',
     cursor: 'pointer',
     transition: 'background-color 0.2s',
-    ':hover': {
-      backgroundColor: 'rgba(126, 201, 135, 0.1)'
-    }
-  }
+  },
 };
