7706abbabffb8154f761f176b2a7408081d2736c
Hussein Abdikarim Hussein <husseinabdikarim18@gmail.com>
2025-06-03 16:39:38 +0300

    login and register removed from sidebar

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -7,8 +7,6 @@ import {
   Apple,
   LineChart,
   Settings,
-  LogIn,
-  UserPlus,
 } from "lucide-react";
 import "./styles/Sidebar.css";
 
@@ -19,8 +17,6 @@ const navItems = [
   { icon: Apple, label: "Ingredients", path: "/ingredients" },
   { icon: LineChart, label: "Progress", path: "/progress" },
   { icon: Settings, label: "Settings", path: "/settings" },
-  { icon: LogIn, label: "Login", path: "/login" },
-  { icon: UserPlus, label: "Register", path: "/register" },
 ];
 
 export default function Sidebar({ isVisible, toggleSidebar, isLargeScreen }) {
